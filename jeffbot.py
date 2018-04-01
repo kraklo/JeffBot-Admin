6a1cadedb71d5958b42fcf66cc8769d015f717fc
Evan Liapakis <awesomethngs@gmail.com>
2018-04-01 15:09:25 +1000

    Comments

--- jeffbot.py
+++ jeffbot.py
@@ -49,7 +49,9 @@ async def add_player(ctx, user_id, name):
         if ext.exists("players.csv", user_id):
             await client.say('Player already exists.')
         else:
+            # Write to players.csv with the player data
             ext.write("players.csv", [user_id, name, 'no', 'nobody'])
+            # Change nickname and role
             user = discord.utils.get(ctx.message.server.members, name=user_id[:-5])
             role = discord.utils.get(ctx.message.server.roles, name="Castaway")
             await client.say("Added user *{}* as *{}*".format(user_id, name))
@@ -75,8 +77,10 @@ async def remove_player(ctx, user_id):
     if "Host" in [role.name for role in ctx.message.author.roles]:
         if ext.exists("players.csv", user_id):
             id = ext.get("players.csv", 1, user_id)[:-5]
+            # Delete player from players.csv
             ext.write("players.csv", [user_id], True)
             await client.say("Removed {} from player list.".format(user_id))
+            # Replace Castaway role with Spectator
             user = discord.utils.get(ctx.message.server.members, name=id)
             spec = discord.utils.get(ctx.message.server.roles, name="Spectator")
             try:
@@ -96,11 +100,13 @@ async def show(ctx, what):
     """Lists either the players in the player list, the players who have voted, or the players who haven't voted"""
     if "Host" in [role.name for role in ctx.message.author.roles]:
         if what == "players":
+            # Store player ids and then print data
             ids = ext.get("players.csv", 1)
             nicks = ext.get("players.csv", 2)
             for item in ids:
                 await client.say("{}: {}, {} tribe".format(item, nicks[ids.index(item)], ext.get("players.csv", 3, item)))
         elif what == "voted":
+            # Get players who have voted
             ids = ext.get("players.csv", 1)
             voted = [ext.get("players.csv", 2, player) for player in ids if ext.voted(player)]
             if not voted:
@@ -111,6 +117,7 @@ async def show(ctx, what):
                 for player in voted:
                     await client.say(player)
         elif what == "not_voted":
+            # Get players who haven't voted
             ids = ext.get("players.csv", 1)
             not_voted = [ext.get("players.csv", 2, player) for player in ids if not ext.voted(player)]
             if not not_voted:
@@ -131,6 +138,7 @@ async def vote_time(ctx, tribe=''):
     """Manually toggle if players can vote or not"""
     if "Host" in [role.name for role in ctx.message.author.roles]:
         if not ext.is_vote_time() and tribe and ext.exists("tribes.csv", tribe):
+            # Toggle vote time and set tribal to tribe
             ext.toggle("vote_time")
             ext.set_tribal(tribe)
             await client.say("Players can now vote.")
@@ -150,8 +158,10 @@ async def vote_time(ctx, tribe=''):
 async def read_votes(ctx):
     """Manually read the votes"""
     if "Host" in [role.name for role in ctx.message.author.roles]:
+        # Toggle vote time if players can still vote
         if ext.is_vote_time():
             ext.toggle("vote_time")
+        # Store a tally of players and the number of votes they have
         tally = {}
         votes = ext.get("players.csv", 4)
         for item in votes:
@@ -160,6 +170,8 @@ async def read_votes(ctx):
                     tally[item] += 1
                 else:
                     tally[item] = 1
+
+        # Print the data in tally
         await client.say("The votes are as follows.")
         for item in tally:
             if tally[item] == 1:
@@ -167,14 +179,17 @@ async def read_votes(ctx):
             else:
                 await client.say("{} has {} votes.".format(item, tally[item]))
 
+        # Calculate who has the most votes
         highest = max(tally.values())
         most = [a for a, b in tally.items() if b == highest]
 
         if len(most) != 1:
+            # Print tie if thre are more than two people with the highest count
             await client.say("We have a tie!")
         else:
             await client.say("{}, the tribe has spoken.".format(most[0]))
             player_id = ext.get("players.csv", 1, most[0])
+            # Replace role to either Spectator or Juror
             user = discord.utils.get(ctx.message.server.members, name=player_id[:-5])
             spec = discord.utils.get(ctx.message.server.roles, name="Spectator")
             if len(ext.get("players.csv", 1)) <= 10:
@@ -185,11 +200,14 @@ async def read_votes(ctx):
                 await client.say("Unable to replace role.")
             except AttributeError:
                 await client.say("Unable to replace role.")
+            # Delete player from player list
             ext.write("players.csv", [player_id], True)
 
+        # Set everyone's vote to nobody
         ids = ext.get("players.csv", 1)
         for item in ids:
             ext.write("players.csv", [item, ext.get("players.csv", 2, item), ext.get_tribal(), 'nobody'])
+        # Reset tribal
         ext.set_tribal('none')
     else:
         await client.say("You are not a host.")
@@ -245,24 +263,30 @@ async def vote(ctx, player):
 async def sort_tribes(ctx, tribe1, tribe2):
     """Sorts players into tribes. (tribe1, tribe2)"""
     if "Host" in [role.name for role in ctx.message.author.roles]:
+        # Store all player data in a dict
         player_data = {}
         tribes = [tribe1, tribe2]
         counter = {tribe1: 0, tribe2: 0}
         players = ext.get("players.csv", 1)
         for player in players:
+            # Choose a random tribe
             while True:
                 choice = random.choice(tribes)
                 if counter[choice] < len(players) / 2:
                     counter[choice] += 1
                     break
+            # Assign tribe to player
             player_data[player] = [
                 player,
                 ext.get("players.csv", 2, player),
                 choice,
                 'nobody'
             ]
+
         for player in player_data:
+            # Write data to players.csv
             ext.write("players.csv", player_data[player])
+            # Change roles
             role = discord.utils.get(
                 ctx.message.server.roles,
                 name=player_data[player][2]
@@ -275,6 +299,10 @@ async def sort_tribes(ctx, tribe1, tribe2):
                 await client.add_roles(user, role)
             except discord.errors.Forbidden:
                 await client.say("Unable to add {} role to {}. Forbidden.".format(player_data[player][2], player_data[player][0]))
+            except AttributeError:
+                await client.say("Unable to add {} role to {}. Role does not exist.".format(player_data[player][2], player_data[player][0]))
+
+        # Write tribes to tribes.csv
         ext.write("tribes.csv", [tribe1, tribe2])
     else:
         await client.say("You are not a host.")
@@ -286,10 +314,12 @@ async def merge_tribes(ctx, tribe):
     if "Host" in [role.name for role in ctx.message.author.roles]:
         ids = ext.get("players.csv", 1)
         for player in ids:
+            # Change tribe to new merge tribe
             ext.write(
                 "players.csv",
                 [player, ext.get("players.csv", 2, player), tribe, 'nobody']
             )
+            # Change roles
             role = discord.utils.get(ctx.message.server.roles, name=tribe)
             castaway = discord.utils.get(
                 ctx.message.server.roles,
@@ -305,7 +335,10 @@ async def merge_tribes(ctx, tribe):
                 await client.say("Forbidden to add role.")
             except AttributeError:
                 await client.say("Role {} does not exist.".format(tribe))
+
+        # Delete old tribes from tribes.csv
         ext.write("tribes.csv", ext.get("tribes.csv", 1)[1], True)
+        # Write new tribe to tribes.csv
         ext.write("tribes.csv", [tribe])
     else:
         await client.say("You are not a host.")
